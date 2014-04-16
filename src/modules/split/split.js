57a7337a4db58a9742239567743cd7d9a21de77f
Pierre Spring <pierre@nelm.io>
2014-04-16 04:01:49 +0200

    started working on the split module

--- src/modules/split/split.js
+++ src/modules/split/split.js
@@ -1,9 +1,167 @@
-define(['jquery'], function () {
+(function () {
 	'use strict';
-	return {
-		init: function ($el) {
-			console.log('Split module is initializeing the following element:');
-			console.log($el[0]);
+	function getRandomColor() {
+		var letters, color, i;
+
+		letters = '0123456789ABCDEF'.split('');
+		color = '#';
+		for (i = 0; i < 6; i += 1) {
+			color += letters[Math.floor(Math.random() * 16)];
 		}
-	};
-});
+		return color;
+	}
+
+	function recalculate($el) {
+
+		var $rows, rowHeight;
+
+		$rows = $el.find('.split-row');
+
+		if (!$rows.length) {
+			return;
+		}
+
+		rowHeight = 100 / $rows.length;
+
+		$rows.each(function (index, el) {
+			var $el, $columns, columnWidth;
+
+			$el = $(el);
+			$el.css('height', rowHeight + '%');
+
+			$columns = $el.find('.split-column');
+
+			if (!$columns.size()) {
+				throw 'Rows should not be empty.';
+			}
+
+			columnWidth = 100 / $columns.size();
+			$columns.each(function (index, el) {
+				var $el;
+				$el = $(el);
+				$el.css('width', columnWidth + '%');
+
+				if (!$el.hasClass('js-split-colored')) {
+					$el.css('background-color', getRandomColor())
+						.addClass('js-split-colored');
+				}
+			});
+		});
+	}
+
+	define(['jquery'], function () {
+		return {
+			init: function ($el) {
+				recalculate($el);
+
+				$el.on('dragstart', '.split-column', function () {
+					$(this).addClass('split-is-dragged');
+				});
+
+				$el.on('dragend', '.split-column', function () {
+					var $this, $overlay, position, $dropTarget, removeRow;
+					$this = $(this);
+					$this.removeClass('split-is-dragged');
+					$overlay = $el.find('.split-column-overlay');
+
+					if (!$overlay.length) {
+						return;
+					}
+
+					$dropTarget = $overlay.parent();
+					position = $overlay.data('position');
+					$overlay.remove();
+
+					removeRow = !$this.siblings().size();
+
+					if (removeRow) {
+						$this.parent().remove();
+					}
+
+					if (position === 'left') {
+						$dropTarget.before($this);
+					} else if (position === 'right') {
+						$dropTarget.after($this);
+					}
+
+					recalculate($el);
+
+				});
+				$el.on('dragleave', '.split-column', function () {
+					$(this).find('.split-column-overlay').remove();
+				});
+
+				$el.on('dragenter dragover', '.split-column:not(.split-is-dragged)', function (e) {
+					var x, y, height, width, offset, $this, distanceToBottom, distanceToRight, $overlay;
+
+					$this = $(this);
+
+					if (e.originalEvent.offsetX) {
+						x = e.originalEvent.offsetX;
+						y = e.originalEvent.offsetY;
+					} else {
+						offset = $this.offset();
+						x = e.originalEvent.pageX - offset.left;
+						y = e.originalEvent.pageY - offset.top;
+					}
+
+					if (x < 0 || y < 0) {
+						return;
+					}
+
+					width = $this.width();
+					height = $this.height();
+
+					distanceToRight = width - x;
+					distanceToBottom = height - y;
+
+					$overlay = $this.find('.split-column-overlay');
+					if (!$overlay.length) {
+						$overlay = $('<div class="split-column-overlay"/>');
+						setTimeout(
+							function () {
+								$this.append($overlay);
+							}, 0
+						);
+					}
+
+					if (x < y && x < distanceToBottom && x < distanceToRight) {
+						$overlay.css({
+							top: 0,
+							bottom: 0,
+							left: 0,
+							right: '50%'
+						}).data('position', 'left');
+					} else if (y < distanceToRight && y < distanceToBottom) {
+						$overlay.css({
+							top: 0,
+							bottom: '50%',
+							left: 0,
+							right: 0
+						}).data('position', 'top');
+					} else if (distanceToBottom < distanceToRight) {
+						$overlay.css({
+							top: '50%',
+							bottom: 0,
+							left: 0,
+							right: 0
+						}).data('position', 'bottom');
+					} else {
+						$overlay.css({
+							top: 0,
+							bottom: 0,
+							left: '50%',
+							right: 0
+						}).data('position', 'right');
+					}
+
+					if (e.preventDefault) {
+						e.preventDefault();
+					}
+					return false;
+				});
+			}
+		};
+	});
+
+}());
